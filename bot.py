a926a3ddfd6648cc92203139a9586eb73ada0b16
YoungHao <younghao5257@gmail.com>
2018-09-05 10:25:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,9 +99,10 @@ def train_dialogue(domain_file="mobile_domain.yml",
 
 
 def train_nlu():
-    from rasa_nlu.converters import load_data
-    from rasa_nlu.config import RasaNLUConfig
+    from rasa_nlu.training_data import load_data
+    from rasa_nlu import config
     from rasa_nlu.model import Trainer
+
 
     training_data = load_data("data/mobile_nlu_data.json")
     trainer = Trainer(RasaNLUConfig("mobile_nlu_model_config.json"))
